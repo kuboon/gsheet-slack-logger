cb2243d312cf13bbc2ccd22a9f812f6564670172
ohkubo kohei <kuboon@trick-with.net>
2021-11-01 20:36:54 +0900

    fix typo

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ export default {
   tz: process.env['INPUT_TIMEZONE']!,
   folder: process.env['INPUT_FOLDERID']!,
   autoJoin: process.env['INPUT_AUTOJOIN'] == 'true',
-  skipChannels: (process.env['INPUT_AUTOJOIN'] || '').split(' '),
+  skipChannels: (process.env['INPUT_SKIPCHANNELS'] || '').split(' '),
   slack: {
     token: process.env['INPUT_SLACKTOKEN']!
   },
