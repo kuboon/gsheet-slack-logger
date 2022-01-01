5b66ac3815a7c9c553273f7afc62cc2e09cdafac
Ohkubo KOHEI <kuboon@trick-with.net>
2022-01-01 12:12:45 +0000

    fix month calc

--- src/monthly.ts
+++ src/monthly.ts
@@ -4,22 +4,28 @@ import * as core from '@actions/core';
 
 import { Temporal } from '@js-temporal/polyfill';
 
-let year = parseInt(core.getInput('year'))
-let month = parseInt(core.getInput('month'))
-if (isNaN(year)) {
-  if (!isNaN(month)) {
-    core.setFailed('should specify both year and month')
-    process.exit(1)
-  }
-  const now = new Date
-  year = now.getFullYear()
-  month = now.getMonth() + 1 - 2
-}
-core.notice(`start backing up ${year}/${month}`)
-
 const timeZone = settings.tz;
-const from = Temporal.ZonedDateTime.from({ timeZone, year, month, day: 1})
-const to = Temporal.ZonedDateTime.from({ timeZone, year, month: month+1, day: 1})
+const year = parseInt(core.getInput('year'));
+const month = parseInt(core.getInput('month'));
+let from
+if (isNaN(year)) {
+    if (!isNaN(month)) {
+        core.setFailed('should specify both year and month');
+        process.exit(1);
+    }
+    const now = Temporal.Now.zonedDateTimeISO(timeZone).toPlainYearMonth();
+    const twoMonths = Temporal.Duration.from({months: 2});
+    from = now.subtract(twoMonths).toPlainDate({day: 1}).toZonedDateTime(timeZone);
+} else {
+    const ym = Temporal.PlainDateTime.from({
+        year,
+        month
+      });
+    from = ym.toZonedDateTime(timeZone)
+}
+core.notice(`start backing up ${from.year}/${from.month}`);
+const to = from.add({months: 1})
+
 main(false, new Date(from.epochMilliseconds), new Date(to.epochMilliseconds)).catch(e => {
   console.error(e)
   core.setFailed(`Action failed with error ${e}`);
